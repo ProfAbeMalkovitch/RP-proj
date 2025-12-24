a9d156dcb456136a4f62ebf4cbb269a3b99ffb28
Iroshan <133976828+ProfAbeMalkovitch@users.noreply.github.com>
2025-12-24 20:58:46 +0530

    Add files via upload

---
+++ src/components/accounts/index.js
@@ -0,0 +1,13 @@
+/**
+ * Account Components Index
+ * Export all account-related components
+ */
+
+export { default as LoginPage } from './LoginPage';
+export { default as RegisterPage } from './RegisterPage';
+export { default as AccountEditPage } from './AccountEditPage';
+
+
+
+
+
