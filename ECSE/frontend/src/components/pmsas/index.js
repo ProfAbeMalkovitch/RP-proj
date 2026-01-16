d596a0c6b07f83a8c6942a50d4934dfbe92172b4
Iroshan <133976828+ProfAbeMalkovitch@users.noreply.github.com>
2026-01-16 20:53:43 +0530

    Delete ECSE directory

--- ECSE/frontend/src/components/pmsas/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * PMSAS Components Index
- * Export all Progress Monitoring and Streak Analytics components
- */
-
-export { default as ProgressDashboard } from './ProgressDashboard';
-export { default as TeacherAnalytics } from './TeacherAnalytics';
